42d5b8ea140cd0c08ccd4025996e29697adad3ef
Sunny Kumar <125676565+SunnySharma01@users.noreply.github.com>
2023-07-28 20:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function fibonacci(num) {
+  if (num < 0 || num > 50) {
+    throw new Error("Input should be between 0 and 50");
+  }
+
+  // Create a memoization object to store computed Fibonacci numbers
+  const memo = {};
+
+  function fib(n) {
+    if (n in memo) {
+      return memo[n];
+    }
+
+    if (n <= 1) {
+      return n;
+    }
+
+    // Calculate the Fibonacci number using recursion and store it in the memoization object
+    memo[n] = fib(n - 1) + fib(n - 2);
+    return memo[n];
+  }
+
+  return fib(num);
+}
+	
 // your code here
 }
 
